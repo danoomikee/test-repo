7446690eaff06f7b740829a461a26d6bb295d879
danoomikee <danoomikee@gmail.com>
2025-05-12 21:18:25 +0300

    CBD - Template

--- packages/ui/src/layouts/LayoutBuilder.tsx
+++ packages/ui/src/layouts/LayoutBuilder.tsx
@@ -9,69 +9,8 @@ import {
   SidebarInset,
   SidebarProvider,
 } from "@workspace/ui/components/sidebar"
-
-
-
-// src/components/layout/LayoutBuilder.tsx
-import { LayoutBuilderProps } from '@workspace/ui/types/layout-config'; // Adjust path
-
-// Import all your component variations
-// Sidebar Headers
-import BasicSidebarHeader from '@workspace/ui/layouts/sidebar-headers/basic-sidebar-header';
-import TeamSwitcher from '@workspace/ui/layouts/sidebar-headers/team-switcher';
-import VersionSwitcher from '@workspace/ui/layouts/sidebar-headers/version-switcher';
-
-// Sidebar Bodies
-import NavMain from '@workspace/ui/layouts/sidebar-bodies/main-nav';
-import NavProjects from '@workspace/ui/layouts/sidebar-bodies/nav-projects';
-import NavSecondary from '@workspace/ui/layouts/sidebar-bodies/nav-secondary';
-import NavFavorites from '@workspace/ui/layouts/sidebar-bodies/nav-favorites';
-import NavWorkspaces from '@workspace/ui/layouts/sidebar-bodies/nav-workspaces';
-
-// Sidebar Footers
-import NavUser from '@workspace/ui/layouts/sidebar-footers/nav-user';
-import SidebarOptInForm, { SidebarOptInFormPadded } from '@workspace/ui/layouts/sidebar-footers/sidebar-opt-in-form';
-
-// Main Headers
-import BasicMainHeader from '@workspace/ui/layouts/main-headers/basic-main-header';
-import HeaderWithSearch from '@workspace/ui/layouts/main-headers/header-with-search';
-
-// Sidebar Wrappers (Optional, if you have structural sidebar variants like DefaultSidebar)
-// import Sidebar07 from "@workspace/ui/layouts/sidebars/sidebar-07"
-// import Sidebar06 from "@workspace/ui/layouts/sidebars/sidebar-06"
-
-
-
-// --- Component Mapping (Example - helps keep the render logic cleaner) ---
-
-const componentRegistry = {
-  sidebarHeader: {
-    basic: BasicSidebarHeader,
-    teamSwitch: TeamSwitcher,
-    versionSwitch: VersionSwitcher,
-    // Add more header variants here...
-  },
-  sidebarBody: {
-    mainNav: NavMain,
-    secondaryNav: NavSecondary,
-    favoritesNav: NavFavorites,
-    projectsNav: NavProjects,
-    workspacesNav: NavWorkspaces,
-
-  },
-  sidebarFooter: {
-    navUser: NavUser,
-    // navOptInForm: SidebarOptInForm,
-    navOptInForm: SidebarOptInFormPadded,
-  },
-  mainHeader: {
-    basic: BasicMainHeader,
-    withSearch: HeaderWithSearch,
-  },
-}
-
-// import DefaultSidebar from './variations/sidebars/DefaultSidebar';
-
+import componentRegistry from "../utils/component-registery.js";
+import { LayoutBuilderProps } from "../types/layout-config.js";
 
 
 export function AppShell({ config, children, ...props }: LayoutBuilderProps & React.ComponentProps<typeof Sidebar>) {
